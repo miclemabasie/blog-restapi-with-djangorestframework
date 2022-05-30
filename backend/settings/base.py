35e16ec8b06d2f0294130eca787ae0f89017cf03
MICLEM <miclemabasie3@gmail.com>
2022-05-30 22:25:20 +0100

    implementing logging

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -2,6 +2,7 @@ import environ
 from pathlib import Path
 import os
 
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
@@ -131,3 +132,59 @@ STATICFILES_DIRS = []
 
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+
+# LOGGING
+import logging
+import logging.config
+
+from django.utils.log import DEFAULT_LOGGING
+
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = 'INFO'
+
+logging.config.dictConfig(
+    {
+    "version":1,
+    "disable_existing_loggers": False,
+    "formatters":{
+        "console":{
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        "file":{
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        "django.server":DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+
+    "handlers":{
+        "console":{
+            "class":"logging.StreamHandler",
+            "formatter":"console",
+        },
+        "file":{
+            "level":"INFO",
+            "class":"logging.FileHandler",
+            "formatter":"file",
+            "filename":"backend/logs/django_blog.log"
+        },
+        "django.server":DEFAULT_LOGGING["handlers"]["django.server"]
+    },
+
+    "loggers":{
+        "":{
+            "level":"INFO",
+            "handlers":["console"],
+            "propagate":False   
+        },
+        "apps":{
+            "level":"INFO",
+            "handler":["console"],
+            "propagate":False
+        },
+        "django.server":DEFAULT_LOGGING["loggers"]["django.server"]
+    }
+    },
+)
---
+++ backend/users/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+
+from django.contrib.auth.models import User
+
